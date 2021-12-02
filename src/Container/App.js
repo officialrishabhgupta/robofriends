d4a1f7a9adf7127b9f2862b61798543399d942a2
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2021-12-02 10:17:32 +0530

    added

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React,{useState,useEffect} from "react";
 import CardList from "../Components/CardList";
 import SearchBox from '../Components/SearchBox';
 import './App.css';
@@ -6,42 +6,38 @@ import ErrorBoundary from "../Components/ErrorBoundary";
 import Scroll from '../Components/Scroll';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state={
-            Robots:[],
-            searchfield:''  
-        }
-    }
+function App() {
+    const [Robots, setRobots] =useState([])
+    const [searchfield, setSearchfield] =useState('')
+    const [count, setCount]=useState(0)
 
-    componentDidMount() {
+    useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> response.json())
-        .then(users => this.setState({Robots:users}));
+        .then(users => {setRobots(users)});
+        console.log(count)
+    },[count]) //only run if count changes.
+
+
+    const onSearchChange = (event)=> {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange=(event)=> {
-        this.setState({searchfield:event.target.value})
+    const filteredRobots = Robots.filter(Robots=>{
+        return Robots.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return(
+        <div className='tc'>
+        <h1 className='f1'>ROBOFRIENDS</h1>
+        <button onClick={()=>setCount(count+1)}>Click Me!</button>
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+            <ErrorBoundary>
+            <CardList Robots={filteredRobots}/>
+            </ErrorBoundary>
+        </Scroll>
+        </div>
+    );
     }
-
-    render(){
-        const {Robots, searchfield}= this.state;
-        const filteredRobots = Robots.filter(Robots=>{
-            return Robots.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return(
-            <div className='tc'>
-            <h1 className='f1'>ROBOFRIENDS</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-                <ErrorBoundary>
-                <CardList Robots={filteredRobots}/>
-                </ErrorBoundary>
-            </Scroll>
-            </div>
-        );
-    }
-}
 
 export default App;
